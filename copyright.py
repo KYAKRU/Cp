678c3ee13fe0f5a8109544361d934d7448da9bb7
Chiranjib <93111779+Koch-Dev@users.noreply.github.com>
2024-01-26 22:11:53 +0530

    Update copyright.py

--- copyright.py
+++ copyright.py
@@ -13,7 +13,6 @@ from pyrogram.enums import ChatMemberStatus, ChatType
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
-
 API_ID = 18551731
 API_HASH = "227df64ada8de8ebad3b3df5a7603d12"
 BOT_TOKEN = "6790998054:AAGJ2sebl3MpWJn7a-MMa8M_HQTydWUuMHY"
@@ -26,17 +25,17 @@ DISABLE_CHATS = []
 GROUP_MEDIAS = {}
 
 DELETE_MESSAGE = [
-"1 Hour complete, I'm doing my work...",
-"Its time to delete all medias!",
-"No one can Copyright until I'm alive 😤",
-"Hue hue, let's delete media...",
-"I'm here to delete medias 🙋", 
-"😮‍💨 Finally I delete medias",
-"Great work done by me 🥲",
-"All media cleared!",
-"hue hue medias deleted by me 😮‍💨",
-"medias....",
-"it's hard to delete all medias 🙄",
+    "1 Hour complete, I'm doing my work...",
+    "Its time to delete all medias!",
+    "No one can Copyright until I'm alive 😤",
+    "Hue hue, let's delete media...",
+    "I'm here to delete medias 🙋", 
+    "😮‍💨 Finally I delete medias",
+    "Great work done by me 🥲",
+    "All media cleared!",
+    "hue hue medias deleted by me 😮‍💨",
+    "medias....",
+    "it's hard to delete all medias 🙄",
 ]
 
 START_MESSAGE = """
@@ -54,52 +53,49 @@ BUTTON = [[InlineKeyboardButton("+ Add me in group +", url="http://t.me/AntiCopy
 RiZoeL = Client('RiZoeL-Anti-CopyRight', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 def add_user(user_id):
-   if user_id not in TOTAL_USERS:
-      TOTAL_USERS.append(user_id)
+    if user_id not in TOTAL_USERS:
+        TOTAL_USERS.append(user_id)
 
 @RiZoeL.on_message(filters.command(["ping", "speed"]))
 async def ping(_, e: Message):
-   start = datetime.datetime.now()
-   add_user(e.from_user.id)
-   rep = await e.reply_text("**Pong !!**")
-   end = datetime.datetime.now()
-   ms = (end-start).microseconds / 1000
-   await rep.edit_text(f"🤖 **PONG**: `{ms}`ᴍs")
+    start = datetime.datetime.now()
+    add_user(e.from_user.id)
+    rep = await e.reply_text("**Pong !!**")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await rep.edit_text(f"🤖 **PONG**: `{ms}`ᴍs")
 
 @RiZoeL.on_message(filters.command(["help", "start"]))
 async def start_message(_, message: Message):
-   add_user(message.from_user.id)
-   await message.reply(START_MESSAGE.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(BUTTON))
+    add_user(message.from_user.id)
+    await message.reply(START_MESSAGE.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(BUTTON))
 
 @RiZoeL.on_message(filters.user(DEVS) & filters.command(["restart", "reboot"]))
 async def restart_(_, e: Message):
-   await e.reply("**Restarting.....**")
-   try:
-      await RiZoeL.stop()
-   except Exception:
-      pass
-   args = [sys.executable, "copyright.py"]
-   os.execl(sys.executable, *args)
-   quit()
+    await e.reply("**Restarting.....**")
+    try:
+        await RiZoeL.stop()
+    except Exception:
+        pass
+    args = [sys.executable, "copyright.py"]
+    os.execl(sys.executable, *args)
+    quit()
 
 @RiZoeL.on_message(filters.user(DEVS) & filters.command(["stat", "stats"]))
 async def status(_, message: Message):
-   wait = await message.reply("Fetching.....")
-   stats = "**Here is total stats of me!** \n\n"
-   stats += f"Total Chats: `{len(ALL_GROUPS)}` \n"
-   stats += f"Total users: `{len(TOTAL_USERS)}` \n"
-   stats += f"Disabled chats: `{len(DISABLE_CHATS)}` \n"
-   stats += f"Total Media active chats: `{len(MEDIA_GROUPS)}` \n\n"
-   #stats += f"**© @RiZoeLX**"
-   await wait.edit_text(stats)
+    wait = await message.reply("Fetching.....")
+    stats = "**Here is total stats of me!** \n\n"
+    stats += f"Total Chats: `{len(ALL_GROUPS)}` \n"
+    stats += f"Total users: `{len(TOTAL_USERS)}` \n"
+    stats += f"Disabled chats: `{len(DISABLE_CHATS)}` \n"
+    stats += f"Total Media active chats: `{len(MEDIA_GROUPS)}` \n\n"
+    await wait.edit_text(stats)
 
-
-   
 @RiZoeL.on_message(filters.command(["anticopyright", "copyright"]))
 async def enable_disable(Rizoel: RiZoeL, message: Message):
     chat = message.chat
     if chat.id == message.from_user.id:
-        await message.reply("Use this command in a group!")
+        await message.reply("Use this command in group!")
         return
     txt = ' '.join(message.command[1:])
     if txt:
@@ -109,27 +105,28 @@ async def enable_disable(Rizoel: RiZoeL, message: Message):
                 if chat.id in DISABLE_CHATS:
                     await message.reply(f"Enabled anti-copyright! for {chat.title}")
                     DISABLE_CHATS.remove(chat.id)
-                    return
-                await message.reply("Already enabled!")
+                else:
+                    await message.reply("Already enabled!")
                 return
         elif re.search("no|off|disable".lower(), txt.lower()):
             if member.status == ChatMemberStatus.OWNER or member.user.id in DEVS:
                 if chat.id in DISABLE_CHATS:
                     await message.reply("Already disabled!")
-                    return
-                DISABLE_CHATS.append(chat.id)
-                if chat.id in MEDIA_GROUPS:
-                    MEDIA_GROUPS.remove(chat.id)
-                await message.reply(f"Disable Anti-CopyRight for {chat.title}!")
+                else:
+                    DISABLE_CHATS.append(chat.id)
+                    if chat.id in MEDIA_GROUPS:
+                        MEDIA_GROUPS.remove(chat.id)
+                    await message.reply(f"Disable Anti-CopyRight for {chat.title}!")
             else:
                 await message.reply("Only chat Owner can disable anti-copyright!")
-                return
+            return
         else:
             if member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR] or member.user.id in DEVS:
                 if chat.id in DISABLE_CHATS:
                     await message.reply("Anti-Copyright is disabled for this chat! \n\ntype `/anticopyright enable` to enable Anti-CopyRight")
                 else:
                     await message.reply("Anti-Copyright is enabled for this chat! \n\ntype `/anticopyright disable` to disable Anti-CopyRight")
+                return
     else:
         if chat.id in DISABLE_CHATS:
             await message.reply("Anti-Copyright is disabled for this chat! \n\ntype `/anticopyright enable` to enable Anti-CopyRight")
@@ -138,48 +135,46 @@ async def enable_disable(Rizoel: RiZoeL, message: Message):
 
 @RiZoeL.on_message(filters.all & filters.group)
 async def watcher(_, message: Message):
-   chat = message.chat
-   user_id = message.from_user.id
-   if chat.type == ChatType.GROUP or chat.type == ChatType.SUPERGROUP:
-      
-
-      if chat.id not in ALL_GROUPS:
-         ALL_GROUPS.append(chat.id)
-      if chat.id in DISABLE_CHATS:
-         return
-      if chat.id not in MEDIA_GROUPS:
-         if chat.id in DISABLE_CHATS:
+    chat = message.chat
+    user_id = message.from_user.id
+    if chat.type == ChatType.GROUP or chat.type == ChatType.SUPERGROUP:
+        if chat.id not in ALL_GROUPS:
+            ALL_GROUPS.append(chat.id)
+        if chat.id in DISABLE_CHATS:
             return
-         MEDIA_GROUPS.append(chat.id)
-      if (message.video or message.photo or message.animation or message.document):
-         check = GROUP_MEDIAS.get(chat.id)
-         if check:
-            GROUP_MEDIAS[chat.id].append(message.id)
-            print(f"Chat: {chat.title}, message ID: {message.id}")
-         else:
-            GROUP_MEDIAS[chat.id] = [message.id]
-            print(f"Chat: {chat.title}, message ID: {message.id}")
+        if chat.id not in MEDIA_GROUPS:
+            if chat.id in DISABLE_CHATS:
+                return
+            MEDIA_GROUPS.append(chat.id)
+        if message.video or message.photo or message.animation or message.document:
+            check = GROUP_MEDIAS.get(chat.id)
+            if check:
+                GROUP_MEDIAS[chat.id].append(message.id)
+                print(f"Chat: {chat.title}, message ID: {message.id}")
+            else:
+                GROUP_MEDIAS[chat.id] = [message.id]
+                print(f"Chat: {chat.title}, message ID: {message.id}")
 
 def AutoDelete():
     if len(MEDIA_GROUPS) == 0:
-       return
+        return
 
     for i in MEDIA_GROUPS:
-       addchat(i)
-       if i in DISABLE_CHATS:
-         return
-       message_list = list(GROUP_MEDIAS.get(i))
-       try:
-          hue = RiZoeL.send_message(i, random.choice(DELETE_MESSAGE))
-          RiZoeL.delete_messages(i, message_list, revoke=True)
-          asyncio.sleep(1)
-          hue.delete()
-          GROUP_MEDIAS[i].delete()
-       except Exception:
-          pass
-    MEDIA_GROUPS.remove(i)
-    print("clean all medias ✓")
-    print("waiting for 1 hour")
+        add_user(i)
+        if i in DISABLE_CHATS:
+            return
+        message_list = list(GROUP_MEDIAS.get(i))
+        try:
+            hue = RiZoeL.send_message(i, random.choice(DELETE_MESSAGE))
+            RiZoeL.delete_messages(i, message_list, revoke=True)
+            await asyncio.sleep(1)
+            await hue.delete()
+            GROUP_MEDIAS.pop(i, None)
+        except Exception:
+            pass
+        MEDIA_GROUPS.remove(i)
+        print("clean all medias ✓")
+        print("waiting for 1 hour")
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(AutoDelete, "interval", seconds=60)
@@ -187,10 +182,10 @@ scheduler.add_job(AutoDelete, "interval", seconds=60)
 scheduler.start()
 
 def starter():
-   print('starting bot...')
-   RiZoeL.start()
-   print('bot Started ✓')
-   idle()
+    print('starting bot...')
+    RiZoeL.start()
+    print('bot Started ✓')
+    idle()
 
 if __name__ == "__main__":
-   starter()
+    starter()
