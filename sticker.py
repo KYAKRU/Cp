c97db532bb40de870cf5ac02886fd6821aa51ad5
Chiranjib <93111779+Koch-Dev@users.noreply.github.com>
2024-01-29 12:27:30 +0530

    Update sticker.py

--- sticker.py
+++ sticker.py
@@ -1,26 +1,27 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-# Define the API key and API hash for your bot
 API_ID = 18551731
 API_HASH = "227df64ada8de8ebad3b3df5a7603d12"
 BOT_TOKEN = "6790998054:AAGJ2sebl3MpWJn7a-MMa8M_HQTydWUuMHY"
-
 app = Client("sticker_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-@filters.private & app.on_message(filters.command("start"))
+private_filter = filters.private & filters.command("start")
+group_filter = filters.group & filters.command("start")
+
+@app.on_message(private_filter)
 def start_private(_, message: Message):
     message.reply_text("I'm a Sticker Detector Bot! Use me in groups to delete stickers.")
 
-@filters.group & app.on_message(filters.command("start"))
+@app.on_message(group_filter)
 def start_group(_, message: Message):
     message.reply_text("I'm a Sticker Detector Bot! I will delete stickers in this group.")
 
-@filters.group & app.on_message(filters.sticker)
+@app.on_message(filters.group & filters.sticker)
 def delete_stickers_group(_, message: Message):
     message.delete()
 
-@filters.private & app.on_message(filters.sticker)
+@app.on_message(filters.private & filters.sticker)
 def delete_stickers_private(_, message: Message):
     message.reply_text("Sticker detected! Use me in groups to delete stickers.")
 
